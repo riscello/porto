b7f20e8d7f8d38eff0769142dae5f15887267338
riscello <rainskiriscello45@gmail.com>
2024-10-18 20:21:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// toggle and responsive navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navLists = document.querySelector("nav")
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    })
+}
+
+navSlide()
+
+// CLEAR FOMR BEFORE UNLOAD
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
